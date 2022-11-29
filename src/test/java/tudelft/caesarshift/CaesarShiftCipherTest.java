64bab9ea4391fd71f29199d923213b7578ec4792
dev-testautomation <dev.testautomation@gmail.com>
2022-11-29 10:33:20 +0100

    Topic 2: Practical exercise #1 and #2

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,22 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.CharArrayWriter;
+
 public class CaesarShiftCipherTest {
+
+    @ParameterizedTest(name = "message={0}, shift={1}, output={2}")
+    @CsvSource({
+            "abc, 1, bcd", "abc, 0, abc", "abc, 26, abc", "abc, 27, bcd",
+            "xyz, 3, abc", "bcd, -1, abc",
+            "a_bc, 1, invalid", "abc, 261, bcd"
+    })
+    public void CaesarTest(String input, Integer shift, String exp_output) {
+        String output = new CaesarShiftCipher().CaesarShiftCipher(input, shift);
+        Assertions.assertEquals(exp_output, output);
+    }
 
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,31 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy GH;
+    @BeforeEach
+    public void initialize() {
+        GH = new GHappy();
+    }
+    @Test
+    public void singleG() {
+        boolean result = GH.gHappy("xxxgxxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @ParameterizedTest(name = "input={0}, happy={1}")
+    @CsvSource({
+            "xxxgxxx, false", "xxggxx, true", "xxggyygxx, false",
+            "xxgggxx, true", "gxxxx, false", "xxxxg, false",
+            "xxxgg, true", "g, false", "gg, true"})
+    public void paramG(String str, boolean expres) {
+        boolean result = GH.gHappy(str);
+        Assertions.assertEquals(expres, result);
+    }
 }
